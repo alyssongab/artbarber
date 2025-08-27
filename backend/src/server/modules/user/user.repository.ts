6209df785d3174621c872c5a62962822503c48c2
Alysson Gabriel <alysson.gabriel61@gmail.com>
2025-08-27 02:48:17 -0400

    feat: add user creation in user repository

--- backend/src/server/modules/user/user.repository.ts
+++ backend/src/server/modules/user/user.repository.ts
@@ -1,4 +1,17 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 import { User } from "@prisma/client";
 
 const prisma = new PrismaClient();
+
+export class UserRepository {
+
+    /**
+     * Cria um novo usuario no banco
+     * @param data Dados para criar o usuario
+     * @returns Promise<User>
+     */
+    async create(data: Prisma.UserCreateInput): Promise<User> {
+        return prisma.user.create({ data });
+    }
+
+}
