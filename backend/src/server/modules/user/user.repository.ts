fd1a9e01f5413a3e9cfc0b973b8d70f1d8cf38e5
Alysson Gabriel <alysson.gabriel61@gmail.com>
2025-08-27 08:50:32 -0400

    feat: add findAll and findByEmail in user repository

--- backend/src/server/modules/user/user.repository.ts
+++ backend/src/server/modules/user/user.repository.ts
@@ -8,10 +8,28 @@ export class UserRepository {
     /**
      * Cria um novo usuario no banco
      * @param data Dados para criar o usuario
-     * @returns Promise<User>
+     * @returns User
      */
     async create(data: Prisma.UserCreateInput): Promise<User> {
         return prisma.user.create({ data });
     }
 
+    /**
+     * Lista usuários
+     * @returns User[]
+     */
+    async findAll(): Promise<User[]> {
+        return prisma.user.findMany();
+    }
+
+    /**
+     * Encontra usuario pelo email
+     * @param email 
+     * @returns User ou null
+     */
+    async findByEmail(email: string): Promise<User | null> {
+        return prisma.user.findUnique({
+            where: { email }
+        });
+    }
 }
