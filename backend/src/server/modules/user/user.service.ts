dbcf5af895b5c07309ab6678c52d7c92732eefdf
Alysson Gabriel <alysson.gabriel61@gmail.com>
2025-08-30 06:26:57 -0400

    feat: implement updateUser and findUser methods in UserService; add removeUndefined utility function

--- backend/src/server/modules/user/user.service.ts
+++ backend/src/server/modules/user/user.service.ts
@@ -1,8 +1,10 @@
+import { Prisma } from "@prisma/client";
 import type { User } from "@prisma/client";
-import type { CreateClientDTO, LoginInput, UserResponseDTO } from "./user.schema.ts";
+import type { CreateClientDTO, LoginInput, UserResponseDTO, UpdateUserDTO } from "./user.schema.ts";
 import { UserRepository } from "./user.repository.ts";
 import * as bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import { removeUndefined } from "../../shared/utils/object.utils.ts";
 
 export class UserService {
 
@@ -19,6 +21,7 @@ export class UserService {
      */
     private toUserResponseDTO(user: User): UserResponseDTO{
         return{
+            id: user.user_id,
             first_name: user.first_name,
             last_name: user.last_name,
             email: user.email,
@@ -51,7 +54,7 @@ export class UserService {
 
     /**
      * Log in
-     * @param data email and passoword
+     * @param data email and password
      * @returns jwt token
      */
     async login(data: LoginInput): Promise<{ accessToken: string }> {
@@ -80,5 +83,30 @@ export class UserService {
     async listUsers(): Promise<UserResponseDTO[]> {
         const users = await this.userRepository.findAll();
         return users.map(user => this.toUserResponseDTO(user));
+    }
+
+    async updateUser(userId: number, data: UpdateUserDTO): Promise<UserResponseDTO> {
+
+        const userExists = await this.userRepository.findById(userId);
+        if(!userExists) throw new Error("Usuário não encontrado.");
+
+        // if password is provided
+        if(data.password){
+            const salt = 10;
+            data.password = await bcrypt.hash(data.password, salt);
+        }
+
+        const cleanData = removeUndefined(data);
+
+        const updatedUser = await this.userRepository.update(userId, cleanData as Prisma.UserUpdateInput);
+
+        return this.toUserResponseDTO(updatedUser);
+    }
+
+    async findUser(userId: number): Promise<UserResponseDTO | null> {
+        const user = await this.userRepository.findById(userId);
+        if(!user) throw new Error("Usuário não encontrado.");
+
+        return this.toUserResponseDTO(user);
     }
 }
