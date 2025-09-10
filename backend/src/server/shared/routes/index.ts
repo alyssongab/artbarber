06d545e9912aeec37ed4381d61fe191b69cbcdb4
Alysson Gabriel <alysson.gabriel61@gmail.com>
2025-09-10 09:33:08 -0400

    feat: add service route

--- backend/src/server/shared/routes/index.ts
+++ backend/src/server/shared/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import express from 'express';
-import usersRouter from "../../modules/user/user.routes.ts";
+import usersRouter from "../../modules/users/user.routes.ts";
+import servicesRoutes from "../../modules/service/services.routes.ts";
 import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
@@ -12,6 +13,7 @@ const __dirname = dirname(__filename);
 const routes = Router();
 
 routes.use('/users', usersRouter);
+routes.use('/services', servicesRoutes);
 routes.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 routes.use(errorHandler);
 
