62251e650904c557107001c92fa1b5cc0f2efa80
Alysson Gabriel <alysson.gabriel61@gmail.com>
2025-09-13 02:59:57 -0400

    feat: add role-based authorization middleware

---
+++ backend/src/server/shared/middlewares/role.authorization.ts
@@ -0,0 +1,21 @@
+import { ForbiddenError } from "../errors/http.errors.ts";
+import type { Request, Response, NextFunction } from "express";
+
+
+/**
+ * Middleware to create a role-based authorization
+ * @param allowedRoles List of roles that are allowed to access this route
+ * @returns An express middleware function
+ */
+const authorize = (...allowedRoles: string[]) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        const user = req.user
+        if(!user || !allowedRoles.includes(user.role)) {
+            return next(new ForbiddenError("Acesso negado. Você não tem permissão para acessar esse recurso."));
+        }
+
+        next();
+    }
+}
+
+export default authorize;
