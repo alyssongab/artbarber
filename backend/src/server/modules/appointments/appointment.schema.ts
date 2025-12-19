78fcc16a552016c0b68df111ec40113402b807d1
Alysson Gabriel <alysson.gabriel61@gmail.com>
2025-12-19 00:38:11 -0400

    fix: remove precision from appointment_time schema

--- backend/src/server/modules/appointments/appointment.schema.ts
+++ backend/src/server/modules/appointments/appointment.schema.ts
@@ -3,7 +3,7 @@ import { AppointmentStatus } from "../../../generated/prisma/client.ts";
 
 export const createAppointmentSchema = z.strictObject({
     appointment_date: z.iso.date("Data inválida"),
-    appointment_time: z.iso.time({ precision: 0 }),
+    appointment_time: z.iso.time(),
     id_client: z.coerce.number().optional().nullable().default(null),
     id_barber: z.coerce.number("ID Inválido"),
     id_service: z.coerce.number("ID Inválido") 
