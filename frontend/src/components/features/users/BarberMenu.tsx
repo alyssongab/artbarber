79e150f47377186745c5b86c14cf7fae40014742
Alysson Gabriel <alysson.gabriel61@gmail.com>
2026-01-06 14:40:01 -0400

    feat: iadd locale in calendar and enhance dialog handling

--- frontend/src/components/features/users/BarberMenu.tsx
+++ frontend/src/components/features/users/BarberMenu.tsx
@@ -2,10 +2,18 @@ import { Calendar } from "lucide-react";
 import { Dialog, DialogTrigger, DialogContent } from "../../ui/dialog";
 import { Calendar as CalendarUI } from "../../ui/calendar";
 import { useState } from "react";
+import { pt } from "date-fns/locale";
 
 export default function BarberMenu(){
     const [date, setDate] = useState<Date | undefined>(new Date());
+    const [isCalendarOpen, setIsCalendarOpen] = useState(false);
+
     const dateSelected = date?.toLocaleDateString('pt-BR');
+
+    const handleDateSelect = (selectedDate: Date | undefined) => {
+        setDate(selectedDate);
+        setIsCalendarOpen(false);
+    };
     return(
         <div className="flex flex-col bg-white border-1 border-black/20 rounded-sm p-3 gap-3">
             <div className="flex items-center gap-3">
@@ -13,15 +21,16 @@ export default function BarberMenu(){
                 <h2 className="mobile:text-xl font-semibold">Selecionar Data</h2>
             </div>
             <div id="barber-calendar">
-                <Dialog>
+                <Dialog open={isCalendarOpen} onOpenChange={setIsCalendarOpen}>
                     <DialogTrigger className="cursor-pointer border-1 border-black/20 min-w-full bg-white text-gray-800 text-lg w-full text-start px-4 py-1 rounded-lg hover:border-black/30">
                         <p className="text-base">Data Selecionada: {dateSelected}</p>
                     </DialogTrigger>
                     <DialogContent className="flex justify-center w-fit p-10">
                           <CalendarUI
                                 mode="single"
+                                locale={pt}
                                 selected={date}
-                                onSelect={setDate}
+                                onSelect={handleDateSelect}
                                 className="rounded-lg border"
                             />
                     </DialogContent>
