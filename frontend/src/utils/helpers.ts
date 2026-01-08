00e87e37cdcf569d2b918fa8639544459bcc84fe
Alysson Gabriel <alysson.gabriel61@gmail.com>
2026-01-08 19:44:04 -0400

    fix: rename formatToISOStandard

--- frontend/src/utils/helpers.ts
+++ frontend/src/utils/helpers.ts
@@ -7,7 +7,7 @@
     return timeStr.slice(0, 5); // HH:MM
   };
 
-  export const formatToISOString = (date: Date): string => {
+  export const formatToISOStandard = (date: Date): string => {
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
