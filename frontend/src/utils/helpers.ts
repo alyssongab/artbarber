21cb2a65d9d8e14892014ca79d8cb0567aff5736
Alysson Gabriel <alysson.gabriel61@gmail.com>
2026-01-08 20:50:11 -0400

    refactor: move status style modifier to helper file (decoupling)

--- frontend/src/utils/helpers.ts
+++ frontend/src/utils/helpers.ts
@@ -23,20 +23,27 @@
     }
   }
 
-  export function canCancelAppointment(date: string, time: string): boolean {
+  export const canCancelAppointment = (date: string, time: string): boolean => {
     const [day, month, year] = date.split('/').map(Number);
     const [hours, minutes] = time.split(':').map(Number);
     
     const appointmentDateTime = new Date(year, month - 1, day, hours, minutes);
     const now = new Date();
-
-  //   console.log('canCancelAppointment:', {
-  //   date,
-  //   time,
-  //   appointmentDateTime: appointmentDateTime.toLocaleString(),
-  //   now: now.toLocaleString(),
-  //   canCancel: appointmentDateTime > now
-  // });
     
     return appointmentDateTime > now;
-}
+  }
+
+  export const getStatusStyles = (status: string) => {
+    const upperStatus = status.toUpperCase();
+    if (upperStatus === 'AGENDADO' || upperStatus === 'PENDENTE') {
+      return 'bg-yellow-100 text-yellow-950 font-semibold border border-yellow-300';
+    }
+    if (upperStatus === 'CANCELADO') {
+      return 'bg-red-100 text-red-950 font-semibold border border-red-300';
+    }
+    if (upperStatus === 'CONCLUÍDO' || upperStatus === 'CONCLUIDO') {
+      return 'bg-green-100 text-emerald-950 font-semibold border border-green-300';
+    }
+    
+    return 'bg-gray-300 text-gray-900 font-medium';
+  };
