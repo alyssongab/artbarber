f44052089acea49fc2f56d46cf7ef3ac99f880d4
Alysson Gabriel <alysson.gabriel61@gmail.com>
2026-01-17 02:35:03 -0400

    feat: update admin barbers page with navigation and action buttons (mock)

--- frontend/src/pages/admin/barbers/index.tsx
+++ frontend/src/pages/admin/barbers/index.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { userService } from "../../../services/api"
 import { BarberResponseDTO } from "../../../types";
+import { Link } from "react-router";
+import { ArrowLeft, SquarePen, Trash2 } from "lucide-react";
 
 export default function AdminBarbersPage() {
 
@@ -19,19 +21,40 @@ export default function AdminBarbersPage() {
     useEffect(() => { fetchBarbers() }, []);
 
     return(
-        <div className="p-4">
-            <h1 className="text-2xl font-bold mb-4">Barbeiros</h1>
-            <div className="space-y-3">
+        <div>
+            <div id='barbers-management' className='flex flex-row items-center justify-center relative'>
+                <Link 
+                    to="/admin/home" 
+                    id='return-admin' 
+                    className='bg-black absolute left-0  p-1 rounded-md'
+                >
+                    <ArrowLeft color='white'/>
+                </Link>
+                <div className='flex gap-2 items-center justify-center'>
+                    <h1 className='text-xl md:text-2xl font-medium'>Barbeiros</h1>
+                </div>
+            </div>
+            <div className="mt-6 space-y-4">
                 {barbers.map(barber => (
-                    <div key={barber.user_id} className="border border-black/20 flex items-center gap-4 p-4 rounded">
-                        <img 
-                            src={barber.thumbnail_url || '/default-avatar.png'} 
-                            alt={barber.full_name}
-                            className="w-16 h-16 rounded-full object-cover"
-                        />
-                        <div>
-                            <p className="font-semibold">Nome: {barber.full_name}</p>
-                            <p className="text-sm">Celular: {barber.phone_number}</p>
+                    <div className="flex justify-between items-center border border-black/20 rounded-md">
+                        <div key={barber.user_id} className="flex items-center gap-4 p-4">
+                            <img
+                                src={barber.thumbnail_url || '/default-avatar.png'}
+                                alt={barber.full_name}
+                                className="w-16 h-16 rounded-full object-cover"
+                            />
+                            <div>
+                                <p className="font-semibold">Nome: {barber.full_name}</p>
+                                <p className="text-sm">Celular: {barber.phone_number}</p>
+                            </div>
+                        </div>
+                        <div className="flex flex-row gap-3 p-4" >
+                            <button className="cursor-pointer" title="Editar">
+                                <SquarePen color="blue"/>
+                            </button>
+                            <button className="cursor-pointer" title="Remover">
+                                <Trash2 color="#D10000"/>
+                            </button>
                         </div>
                     </div>
                 ))}
