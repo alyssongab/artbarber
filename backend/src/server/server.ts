601b69f8b53c33f3d1c22a28c30b41144b30b79f
Alysson Gabriel <alysson.gabriel61@gmail.com>
2026-01-21 11:09:45 -0400

    refactor: update server port configuration and add host parameter

--- backend/src/server/server.ts
+++ backend/src/server/server.ts
@@ -3,9 +3,10 @@ import app from "./shared/http/app.ts";
 import { initializeNotificationService } from "./modules/notification/notification.service.ts";
 import { appointmentAutoCancelService } from './modules/appointments/appointment-cron.service.ts';
 
-const PORT = process.env.PORT_BACKEND;
+const PORT = process.env.PORT ? Number(process.env.PORT_BACKEND) : 3030;
+const HOST = '0.0.0.0';
 
-app.listen(PORT, () => {
+app.listen(PORT, HOST, () => {
     console.log(`servidor on na porta ${PORT}`);
     if (process.env.NOTIFICATIONS_ENABLED === 'true') {
         initializeNotificationService();
